cb65460a4b7f395a2c6eadb481a1c26111d98afa
Abdul Harish Khan <khan.abdulharish@outlook.com>
2026-04-01 22:19:27 +0530

    Add Footer component with basic structure

---
+++ Mern Challenges/ecommerece/src/layouts/Footer.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Footer() {
+    return ( 
+        <div>
+            <Footer>
+                <div className></div>
+            </Footer>
+        </div>
+     );
+}
+
+export default Footer;
