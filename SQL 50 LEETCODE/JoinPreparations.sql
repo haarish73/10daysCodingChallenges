7fe708620fda7cdb842febfa226bf297332f138a
Abdul Harish Khan <khan.abdulharish@outlook.com>
2026-04-04 19:27:59 +0530

    Add SQL script for customer and order tables with sample data and queries

---
+++ SQL 50 LEETCODE/JoinPreparations.sql
@@ -0,0 +1,140 @@
+-- mysql> create table customers(customer_id INT PRIMARY KEY,
+--     -> customer_name varchar(50));
+-- Query OK, 0 rows affected (0.07 sec)
+
+-- mysql> show tables;
+-- +-------------------+
+-- | Tables_in_haarish |
+-- +-------------------+
+-- | customers         |
+-- +-------------------+
+-- 1 row in set (0.02 sec)
+
+-- mysql> select * from customers;
+-- Empty set (0.01 sec)
+
+-- mysql> desc customers;
+-- +---------------+-------------+------+-----+---------+-------+
+-- | Field         | Type        | Null | Key | Default | Extra |
+-- +---------------+-------------+------+-----+---------+-------+
+-- | customer_id   | int         | NO   | PRI | NULL    |       |
+-- | customer_name | varchar(50) | YES  |     | NULL    |       |
+-- +---------------+-------------+------+-----+---------+-------+
+-- 2 rows in set (0.01 sec)
+
+-- mysql> create table orders(order_id int PRIMARY KEY, customer_id int, Product_name varchar(50), amount BIGINT, date date);
+-- Query OK, 0 rows affected (0.04 sec)
+
+-- mysql> desc orders;
+-- +--------------+-------------+------+-----+---------+-------+
+-- | Field        | Type        | Null | Key | Default | Extra |
+-- +--------------+-------------+------+-----+---------+-------+
+-- | order_id     | int         | NO   | PRI | NULL    |       |
+-- | customer_id  | int         | YES  |     | NULL    |       |
+-- | Product_name | varchar(50) | YES  |     | NULL    |       |
+-- | amount       | bigint      | YES  |     | NULL    |       |
+-- | date         | date        | YES  |     | NULL    |       |
+-- +--------------+-------------+------+-----+---------+-------+
+-- 5 rows in set (0.00 sec)
+
+-- mysql> INSERT INTO Customers VALUES
+--     -> (1, 'Harish'),
+--     -> (2, 'Rahul'),
+--     -> (3, 'Priya'),
+--     -> (4, 'Sneha'),
+--     -> (5, 'Aman');
+-- Query OK, 5 rows affected (0.01 sec)
+-- Records: 5  Duplicates: 0  Warnings: 0
+
+-- mysql> INSERT INTO Orders VALUES
+--     -> (101, 1, 'Laptop', 70000, '2026-04-01'),
+--     -> (102, 1, 'Mouse', 1000, '2026-04-02'),
+--     -> (103, 3, 'Keyboard', 2000, '2026-04-02'),
+--     -> (104, 4, 'Phone', 30000, '2026-04-03'),
+--     -> (105, 4, 'Charger', 1500, '2026-04-03');
+-- Query OK, 5 rows affected (0.01 sec)
+-- Records: 5  Duplicates: 0  Warnings: 0
+
+-- mysql> select * from customers;
+-- +-------------+---------------+
+-- | customer_id | customer_name |
+-- +-------------+---------------+
+-- |           1 | Harish        |
+-- |           2 | Rahul         |
+-- |           3 | Priya         |
+-- |           4 | Sneha         |
+-- |           5 | Aman          |
+-- +-------------+---------------+
+-- 5 rows in set (0.00 sec)
+
+-- mysql> select * from orders;
+-- +----------+-------------+--------------+--------+------------+
+-- | order_id | customer_id | Product_name | amount | date       |
+-- +----------+-------------+--------------+--------+------------+
+-- |      101 |           1 | Laptop       |  70000 | 2026-04-01 |
+-- |      102 |           1 | Mouse        |   1000 | 2026-04-02 |
+-- |      103 |           3 | Keyboard     |   2000 | 2026-04-02 |
+-- |      104 |           4 | Phone        |  30000 | 2026-04-03 |
+-- |      105 |           4 | Charger      |   1500 | 2026-04-03 |
+-- +----------+-------------+--------------+--------+------------+
+-- 5 rows in set (0.00 sec)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+-- Q1. Show all customers with their orders
+
+-- Return:
+
+-- customer_name
+-- product_name
+
+mysql> select c.customer_name, o.product_name
+    -> From customers c
+    -> INNER JOIN orders o
+    -> ON c.customer_id = o.customer_id;
+-- +---------------+--------------+
+-- | customer_name | product_name |
+-- +---------------+--------------+
+-- | Harish        | Laptop       |
+-- | Harish        | Mouse        |
+-- | Priya         | Keyboard     |
+-- | Sneha         | Phone        |
+-- | Sneha         | Charger      |
+-- +---------------+--------------+
+
+
+-- Q2. Find customers who never placed any order
+
+-- Return:
+
+-- customer_id
+-- customer_name
+
+-- 👉 Use LEFT JOIN + NULL
+
+mysql> SELECT c.customer_id, c.customer_name
+    -> FROM Customers c
+    -> LEFT JOIN Orders o
+    -> ON c.customer_id = o.customer_id
+    -> WHERE o.order_id IS NULL;
+-- +-------------+---------------+
+-- | customer_id | customer_name |
+-- +-------------+---------------+
+-- |           2 | Rahul         |
+-- |           5 | Aman          |
+-- +-------------+---------------+
+
